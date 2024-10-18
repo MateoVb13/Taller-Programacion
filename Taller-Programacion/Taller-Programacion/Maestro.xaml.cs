ff5ff4f96ac644e0c650457cbb564b901beca05c
luchy0 <lucia.arroyo@cecar.edu.co>
2024-10-18 08:03:12 -0500

    Se completaron las funcionalidades de los botones

--- Taller-Programacion/Taller-Programacion/Maestro.xaml.cs
+++ Taller-Programacion/Taller-Programacion/Maestro.xaml.cs
@@ -6,4 +6,31 @@ public partial class NewPage1 : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void goToPage(ContentPage page)
+    {
+        App.FlyoutPage.Detail.Navigation.PushAsync(page);
+        App.FlyoutPage.IsPresented = true;  // Cierra el menú Flyout después de la navegación
+    }
+
+    private void OnNavigateToFetchPage(object sender, EventArgs e)
+    {
+        goToPage(new Pages.FetchPage());
+    }
+
+    private void OnNavigateToPushPage(object sender, EventArgs e)
+    {
+        goToPage(new Pages.PushPage());
+    }
+
+    private void OnNavigateToPullPage(object sender, EventArgs e)
+    {
+        goToPage(new Pages.PullPage());
+    }
+
+    private void OnNavigateToCommitPage(object sender, EventArgs e)
+    {
+        goToPage(new Pages.CommitPage());
+
+    }
 }
